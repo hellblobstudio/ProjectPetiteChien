0cc479fc5efc6d32dd9035cfdea396e80edbf97c
klaegkev <klaegkev@students.zhaw.ch>
2019-05-14 22:41:11 +0200

    initial godot commit

---
+++ projectpetitechien/Camera.gd
@@ -0,0 +1,11 @@
+extends Camera2D
+
+var player;
+
+func _ready():
+	player = get_parent().find_node("Player")
+	print(player)
+	
+
+func _physics_process(delta):
+	self.global_position = lerp(self.global_position, player.global_position, 0.1)
---
+++ projectpetitechien/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using Godot;
+using System;
+
+public class PlayerMovement : KinematicBody2D
+{
+    public float gravity = 9.81f;
+    public float speed = 200f;
+    public int frameRate = 60;
+
+    private Vector2 motion;
+
+    public override void _Ready()
+    {
+        motion = new Vector2();
+        gravity *= frameRate;
+        speed *= frameRate;
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        motion.y += gravity * delta;
+
+        if(Input.IsActionPressed("WalkLeft"))
+        {
+            motion.x = -speed * delta;
+        }
+        if(Input.IsActionPressed("WalkRight"))
+        {
+            motion.x = speed * delta;
+        }
+        if(Input.IsActionPressed("Jump"))
+        {
+            
+        }
+        this.MoveAndSlide(motion, new Vector2(0, -1f));
+
+        motion.x = 0;
+    }
+}
