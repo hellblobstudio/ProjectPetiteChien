9e380dea12b160e6a19a083d08f5334fc74b961c
klaegkev <klaegkev@students.zhaw.ch>
2019-05-16 10:08:46 +0200

    unity test

---
+++ unity/ProjectPetiteChien/Assets/Camera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+   public GameObject player;
+
+   private void FixedUpdate()
+   {
+      this.transform.position = new Vector3(Mathf.Lerp(this.transform.position.x, player.transform.position.x, 0.1f), Mathf.Lerp(this.transform.position.y, player.transform.position.y, 0.1f), -10f);
+   }
+}
---
+++ unity/ProjectPetiteChien/Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+   public float speed = 100f;
+   private Vector2 motion = new Vector2();
+
+   private Rigidbody2D rb;
+
+   private void Start()
+   {
+      this.rb = this.GetComponent<Rigidbody2D>();
+   }
+   private void Update()
+   {
+      motion.x = Input.GetAxis("Horizontal") * speed;
+      Debug.Log(motion.x);
+   }
+
+   private void FixedUpdate()
+   {
+      this.rb.AddForce(motion);
+   }
+}
