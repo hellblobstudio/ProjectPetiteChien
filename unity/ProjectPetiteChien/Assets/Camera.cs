5cad4a50172f1403be07ef01524219f254f65001
klaegkev <klaegkev@students.zhaw.ch>
2019-05-16 10:48:46 +0200

    test

--- unity/ProjectPetiteChien/Assets/Camera.cs
+++ unity/ProjectPetiteChien/Assets/Camera.cs
@@ -6,6 +6,11 @@ public class Camera : MonoBehaviour
 {
    public GameObject player;
 
+   private void Awake()
+   {
+      Application.targetFrameRate = 60;
+   }
+
    private void FixedUpdate()
    {
       this.transform.position = new Vector3(Mathf.Lerp(this.transform.position.x, player.transform.position.x, 0.1f), Mathf.Lerp(this.transform.position.y, player.transform.position.y, 0.1f), -10f);
